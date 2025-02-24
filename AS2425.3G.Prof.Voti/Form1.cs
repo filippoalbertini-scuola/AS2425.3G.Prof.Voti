95553d85b8222d473af2ee47b3fc6a3fc8bfad11
Filippo Albertini <filippo@filippoalbertini.it>
2025-02-24 08:48:48 +0100

    feat: add data structure

--- AS2425.3G.Prof.Voti/Form1.cs
+++ AS2425.3G.Prof.Voti/Form1.cs
@@ -2,6 +2,14 @@ namespace AS2425._3G.Prof.Voti
 {
     public partial class Form1 : Form
     {
+        private const int MAX_VOTI = 100;
+
+        // struttura dati vettori paralleli
+        double[] voti = new double[MAX_VOTI];
+        double[] pesi = new double[MAX_VOTI];
+        string[] materie = new string[MAX_VOTI];
+
+        int numVoti = 0;
         public Form1()
         {
             InitializeComponent();

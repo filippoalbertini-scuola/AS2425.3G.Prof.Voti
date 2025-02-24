f8758131e1f01761d4e344758af0021700006953
Filippo Albertini <filippo@filippoalbertini.it>
2025-02-24 08:28:44 +0100

    Aggiungere i file di progetto.

---
+++ AS2425.3G.Prof.Voti/Form1.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace AS2425._3G.Prof.Voti
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(694, 479);
+            Name = "Form1";
+            Text = "Prof;Lab2.12;24/02/25; Pesi";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ AS2425.3G.Prof.Voti/Form1.cs
@@ -0,0 +1,10 @@
+namespace AS2425._3G.Prof.Voti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
